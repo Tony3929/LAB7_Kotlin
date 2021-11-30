f7bd4f0bcd1428fb5146be1ee429e5020d9df00f
Tony3929 <t108360109@ntut.org.tw>
2021-11-30 20:17:25 +0800

    first

---
+++ app/src/main/java/com/example/lab7_kotlin/AdapterVerticalActivity.kt
@@ -0,0 +1,19 @@
+package com.example.lab7_kotlin
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+
+class adapter_vertical : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_adapter_vertical)
+
+
+
+
+
+    }
+}
